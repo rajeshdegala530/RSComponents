f6d0e2ff1f1133690f17f7b2e3d9d1ba9cdaf88f
rajeshdegala530 <rajeshdegala@gmail.com>
2020-07-12 15:21:21 +0100

    Home test & Search test

---
+++ page-objects/App.js
@@ -0,0 +1,17 @@
+class App {
+
+    openHomePage() {
+        browser.url('https://uk.rs-online.com/web/')
+        browser.setWindowSize(1920, 1080)
+    }
+
+    popUpWindowPresent() {
+        if ($('#fsrFocusFirst').isDisplayed()) {
+            const experienceWindow = $('#fsrFocusFirst')
+            experienceWindow.click()
+        }
+
+    }
+}
+
+export default new App()
